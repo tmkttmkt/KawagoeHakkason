4039419d8ef236b3b4b187e013adb5fa86510aa6
tmkttmkt <24rd123@ms.dendai.ac.jp>
2024-09-01 21:57:39 +0900

    すべてリセットして環境構築

---
+++ back/api/index.js
@@ -0,0 +1,28 @@
+//import
+const express = require('express');
+const cors = require('cors');
+
+//本体
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//ハードより。いろいろアクセスできるようにする
+app.use(cors());
+app.use(express.static('uploads'));
+//APIインポート
+//const personalAPI = require('./personal.js');
+const photoAPI = require('./photo.js');
+//const postedAPI = require('./posted.js');
+const profileAPI = require('./profile.js');
+//app.use("/personal",personalAPI);
+app.use("/photo",photoAPI);
+//app.use("/posted",postedAPI);
+app.use("/profile",profileAPI);
+
+//http//の画面
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  });
+
+//実行
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ back/api/profile.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const router = express.Router();
+
+router.post('/uploadee', (req, res) => {
+    console.log("suygge")
+});
+
+module.exports = router;
