22463697703f535af1d21439841620b8d18d0a0d
DESKTOP-5J2G9A1\24RD164的場 <24rd164@ms.dendai.ac.jp>
2024-09-02 23:03:57 +0900

    写真や位置の追加

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,4 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server:{
+    host:"0.0.0.0",
+    post:3000
+  }
 })
